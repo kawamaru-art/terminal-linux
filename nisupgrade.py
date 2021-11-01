8492259c8e867d634db03ddc8953e078e276eadc
Alanlol <81538835+alanlol12@users.noreply.github.com>
2021-11-01 20:08:03 +0800

    Update and rename nisupdate.py to nisupgrade.py

--- nisupgrade.py
+++ nisupgrade.py
@@ -1,7 +1,7 @@
 import os
 from time import sleep
 os.system("cls")
-files = open("user/share/jasp/username.txt",'r')
+files = ("your username: ")
 os.system("git clone https://github.com/Yeahboi12356/update && cp update/terminal.py home/" + files)
 print("clik y to continue")
 os.system("rd /s update")
