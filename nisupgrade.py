847e41aaa48497da55e109c56019227230c9b506
Alanlol <81538835+alanlol12@users.noreply.github.com>
2021-11-01 20:37:42 +0800

    Update nisupgrade.py

--- nisupgrade.py
+++ nisupgrade.py
@@ -1,7 +1,7 @@
 import os
 from time import sleep
 os.system("cls")
-files = ("your username: ")
+files = input("your username: ")
 os.system("git clone https://github.com/Yeahboi12356/update && cp update/terminal.py home/" + files)
 print("clik y to continue")
 os.system("rd /s update")
