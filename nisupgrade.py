4e264451ce5f45062283dea9b7c6c273c9c559bb
Alanlol <81538835+alanlol12@users.noreply.github.com>
2021-11-02 16:21:29 +0800

    Update nisupgrade.py

--- nisupgrade.py
+++ nisupgrade.py
@@ -1,10 +1,18 @@
 import os
 from time import sleep
-os.system("cls")
-files = input("your username: ")
-os.system("git clone https://github.com/Yeahboi12356/update && cp update/terminal.py home/" + files)
-print("clik y to continue")
-os.system("rd /s update")
-print("restarting terminal..")
-sleep(5)
-os.system("py start-terminal.py")
+print("\nnew pankages: camera-security,version,default")
+n = input("are you sure to upgrade pankages? y/n")
+if n == "y":
+  files = input("your username: ")
+  os.system("git clone https://github.com/Yeahboi12356/update && cp update/terminal.py home/" + files)
+  print("clik y to continue")
+  os.system("rd /s update")
+  print("restarting terminal..")
+  sleep(5)
+  os.system("py start-terminal.py")
+elif n == "n":
+  print("restarting terminal")
+  os.system("py start-terminal.py")
+else:
+    print("number not valid")
+    os.system("py nisupgrade.py")
