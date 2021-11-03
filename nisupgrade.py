7b74186c727b8a17589f35c7cf7be83be3cdef85
Alanlol <81538835+alanlol12@users.noreply.github.com>
2021-11-03 16:30:15 +0800

    Update nisupgrade.py

--- nisupgrade.py
+++ nisupgrade.py
@@ -4,12 +4,7 @@ print("\ncamera-security,version,default\n")
 n = input("are you sure to upgrade pankages? y/n: ")
 if n == "y":
   files = input("your username: ")
-  os.system("git clone https://github.com/Yeahboi12356/update && cp update/terminal.py home/" + files)
-  print("clik y to continue")
-  os.system("rd /s update")
-  print("restarting terminal..")
-  sleep(5)
-  os.system("py start-terminal.py")
+  os.system("git clone https://github.com/Yeahboi12356/update && cp update/terminal.py home/" + files + "cp update/delnis delnis.py)
 elif n == "n":
   print("restarting terminal")
   os.system("py start-terminal.py")
