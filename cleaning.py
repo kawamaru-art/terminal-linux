02ea3d6199b56e8fef92d5ed578ecdbaf13ea048
Alanlol <81538835+alanlol12@users.noreply.github.com>
2021-11-13 15:02:43 +0800

    Delete cleaning.py

--- cleaning.py
+++
@@ -1,5 +0,0 @@
-import os
-
-files = open("user/share/jasp/username.txt",'r')
-os.system("cd home && copy terminal.py " + files.readline())
-print("start terminal with command 'python start-terminal.py' ")
