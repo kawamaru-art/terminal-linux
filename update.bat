de31666a171422a38b5aab025b85f8b8398b859b
Alanlol <81538835+alanlol12@users.noreply.github.com>
2022-02-24 22:21:44 +0800

    Update update.bat

--- update.bat
+++ update.bat
@@ -4,5 +4,5 @@ set /p NAME= What's your user name?:
 cls
 del home\terminal.py
 del home\%NAME%\terminal.py
-copy "update/terminal.py" home\%NAME%
+copy "update\terminal.py" home\%NAME%
 python uipdate.py
